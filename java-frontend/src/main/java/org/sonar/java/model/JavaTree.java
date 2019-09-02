f6153875668d8de03311544f7ee55e55f47ade5e
Evgeny Mandrikov <mandrikov@gmail.com>
2019-09-02 23:00:05 +0200

    (WIP) set root

--- java-frontend/src/main/java/org/sonar/java/model/JavaTree.java
+++ java-frontend/src/main/java/org/sonar/java/model/JavaTree.java
@@ -57,6 +57,7 @@ import org.sonar.sslr.grammar.GrammarRuleKey;
 
 public abstract class JavaTree implements Tree {
 
+  protected CompilationUnitTreeImpl root;
   @Nullable
   private Tree parent;
 
@@ -118,6 +119,7 @@ public abstract class JavaTree implements Tree {
   }
 
   public void setParent(Tree parent) {
+    this.root = ((JavaTree) parent).root;
     this.parent = parent;
   }
 
@@ -163,6 +165,7 @@ public abstract class JavaTree implements Tree {
     public CompilationUnitTreeImpl(@Nullable PackageDeclarationTree packageDeclaration, List<ImportClauseTree> imports, List<Tree> types,
       @Nullable ModuleDeclarationTree moduleDeclaration, SyntaxToken eofToken) {
       super(Kind.COMPILATION_UNIT);
+      this.root = this;
       this.packageDeclaration = packageDeclaration;
       this.imports = imports;
       this.types = types;
